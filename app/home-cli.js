d03a762bc54c644545edd478cc7248b6c16eb127
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-04-07 23:34:00 -0300

    ✨ feat: Início da página principal da visão do cliente.

---
+++ app/home-cli.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Body, Call } from './styles/home-cli';
+
+export default function HomeCli() {
+    return (
+        <Body>
+            <Call>Let's make your dream travel come true!</Call>
+        </Body>
+    );
+}
---
+++ app/styles/home-cli.js
@@ -0,0 +1,22 @@
+import styled from "styled-components/native";
+
+export const Body = styled.View`
+    display: flex;
+    flex-direction: column;
+    background-color: #F6F7FB;
+    height: 100%;
+    width: 100%;
+`;
+
+export const Call = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: black;
+    margin-top: 100px;
+    margin-left: 20px;
+    width: 250px;
+`
+
+export const SearchContainer = styled.View`
+    
+`	
